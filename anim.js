d12047f7ff5349565a1c6a96e188ede6f5a60178
tonykle <tonyl3@uw.edu>
2015-12-08 21:14:04 -0800

    some edits, improvements

--- anim.js
+++ anim.js
@@ -1,22 +1,12 @@
 // This is the JS file I use to make my portfolio interactive.
 
 (function () {
-     $(document).ready(function () {
+     $(document).ready(() => {
         $("html").fadeTo(900, 1.0);
 
-        $("#dribbb").mouseenter(function () {
-            $(this).fadeTo(100, 0.6);
-        });
-
-        $("#dribbb").mouseout(function () {
-            $(this).fadeTo(100, 1.0);
-        });
-
-        $("#linkedin, #res, #github, #uw").mouseenter(function () {
+        $("#linkedin, #res, #github, #uw").mouseenter(function() {
             $(this).fadeTo(100, 0.5);
-        });
-
-        $("#linkedin, #res, #github, #uw").mouseleave(function () {
+        }).mouseleave(function () {
             $(this).fadeTo(0, 1.0);
         });
      });
