6f5e2f5f41deba24e1b1c19eb7bf3aa952bb505f
tonykle <tonyl3@uw.edu>
2015-12-25 02:54:01 -0800

    significant folder structure change, also adding grunt to manage tasks

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      js: {
+        files: ['public/css/index.css'],
+        tasks: ['cssmin'],
+      },
+    },
+  }
+);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['cssmin', 'watch', 'clean']);
+};
