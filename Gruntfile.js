07dd404d81e4965af3991518f1acd80c9c537965
tonykle <tonyl3@uw.edu>
2015-12-25 09:15:31 -0800

    updated readme.md, fixed links

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function(grunt) {
   }
 );
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['cssmin', 'watch', 'clean']);
+  grunt.registerTask('default', ['cssmin', 'watch']);
 };
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tonykle.github.io
+
+This is the repository used for my portfolio website. In it, I show case my knowledge
+of jQuery, D3.js, ES6, and API utilization. I use Grunt to automate tasks.
