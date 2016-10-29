85bd89bad8f0ecc0ef1e5e257364c7955967e743
tonyl3@uw.edu <tonyl3@uw.edu>
2016-10-28 20:20:18 -0400

    fixed date js and css

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@
 window.onload = () => {
   let images =
   [
-    { url : 'http://i.imgur.com/PInwDSB.jpg?1', locationAndDate : 'San Francisco, CA - 08/15/2016' },
-    { url : 'http://i.imgur.com/wtSdnvZ.jpg', locationAndDate : 'San Francisco, CA - 08/15/2016' },
-    { url : 'http://i.imgur.com/tQaufLI.jpg', locationAndDate : 'San Francisco, CA - 08/14/2016'}
-
+    { url : 'http://i.imgur.com/PInwDSB.jpg?1',
+      location : 'San Francisco, CA',
+      date : '08/15/2016'
+    },
+    { url : 'http://i.imgur.com/wtSdnvZ.jpg',
+      location : 'San Francisco, CA',
+      date : '08/15/2016'
+    },
+    { url : 'http://i.imgur.com/tQaufLI.jpg',
+      location : 'San Francisco, CA',
+      date : '08/14/2016'
+    }
   ];
   loadImageLinks();
 
@@ -16,19 +24,25 @@ window.onload = () => {
     images.forEach(imageInfo => {
       let imageContainer = doc.createElement('div');
 
-      // Image title
-      let imageTitle = doc.createElement('div');
-      imageTitle.setAttribute('class', 'imageTitles');
-      imageTitle.innerHTML = imageInfo.locationAndDate;
+      // Image location
+      let imageLocation= doc.createElement('div');
+      imageLocation.setAttribute('class', 'imageTitles');
+      imageLocation.innerHTML = imageInfo.location;
+
+      // Image date
+      let imageDate = doc.createElement('div');
+      imageDate.setAttribute('class', 'imageDates');
+      imageDate.innerHTML = imageInfo.date;
 
       // Image itself
       let image = doc.createElement('img');
       image.setAttribute('class', 'allImages');
       image.setAttribute('src', imageInfo.url);
-      image.setAttribute('alt', 'image-' + imageInfo.locationAndDate);
+      image.setAttribute('alt', 'image-' + imageInfo.location + imageInfo.date);
 
       // Append everything
-      imagesContainer.appendChild(imageTitle);
+      imagesContainer.appendChild(imageLocation);
+      imagesContainer.appendChild(imageDate);
       imagesContainer.appendChild(image);
     });
     doc.getElementById('content').appendChild(imagesContainer);
