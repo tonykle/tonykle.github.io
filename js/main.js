1bf978fef87083a8a2c8935dde9425743856d47a
tonyl3@uw.edu <tonyl3@uw.edu>
2018-01-04 19:01:36 -0500

    redo

--- js/main.js
+++ js/main.js
@@ -1,51 +1,10 @@
-// all images have width of 800px
-
 window.onload = () => {
-  let images =
-  [
-    { url : 'http://i.imgur.com/PInwDSB.jpg?1',
-      location : 'San Francisco, CA',
-      date : 'August 2016'
-    },
-    { url : 'http://i.imgur.com/wtSdnvZ.jpg',
-      location : 'San Francisco, CA',
-      date : 'August 2016'
-    },
-    { url : 'http://i.imgur.com/tQaufLI.jpg',
-      location : 'San Francisco, CA',
-      date : 'August 2016'
-    }
-  ];
-  loadImageLinks();
-
-  function loadImageLinks() {
-    let doc = document;
-    let imagesContainer = doc.createElement('div');
-
-    images.forEach(imageInfo => {
-      let imageContainer = doc.createElement('div');
-
-      // Image location
-      let imageLocation = doc.createElement('div');
-      imageLocation.setAttribute('class', 'imageTitles');
-      imageLocation.innerHTML = imageInfo.location;
-
-      // Image date
-      let imageDate = doc.createElement('div');
-      imageDate.setAttribute('class', 'imageDates');
-      imageDate.innerHTML = imageInfo.date;
-
-      // Image itself
-      let image = doc.createElement('img');
-      image.setAttribute('class', 'allImages');
-      image.setAttribute('src', imageInfo.url);
-      image.setAttribute('alt', 'image-' + imageInfo.location + imageInfo.date);
-
-      // Append everything
-      imagesContainer.appendChild(imageLocation);
-      imagesContainer.appendChild(imageDate);
-      imagesContainer.appendChild(image);
-    });
-    doc.getElementById('content').appendChild(imagesContainer);
-  };
+  let envelope = document.getElementById('envelope');
+  envelope.onclick = () => {
+    window.location = "mailto:tonyl3@uw.edu";
+  }
+  let linkedinlogo = document.getElementById('linkedinlogo');
+  linkedinlogo.onclick = () => {
+    window.location.href = "www.linkedin.com/in/tony-le-01140b79";
+  }
 };
