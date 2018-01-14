6ad697037768772f27c6db4fddad8ccc44998d86
tonyl3@uw.edu <tonyl3@uw.edu>
2018-01-14 08:36:17 -0500

    added usage of Open Weather Map API to display NYC weather stats

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 window.onload = () => {
   // open up email upon clicking
-  let envelope = document.getElementById('envelope');
-  envelope.onclick = () => {
-    window.location = "mailto:tonyl3@uw.edu";
+  document.getElementById('envelope').onclick = () => {
+    window.location = 'mailto:tonyl3@uw.edu';
   }
 
   // redirecting to linked in profile upon clicking linkedin logo
-  let linkedinlogo = document.getElementById('linkedinlogo');
-  linkedinlogo.onclick = () => {
-    location.href = "http://www.linkedin.com/in/tony-le-01140b79";
+  document.getElementById('linkedinlogo').onclick = () => {
+    location.href = 'http://www.linkedin.com/in/tony-le-01140b79';
   }
 
   // date appendage
@@ -21,4 +19,22 @@ window.onload = () => {
   })();
   let fullDate = month + '.' + date.getDate();
   document.getElementById('date').innerHTML = fullDate;
+
+  // Invoking Open Weather Map's API to retrieve weather statistics in NYC
+  $.ajax({
+    type: 'GET',
+    url: 'http://api.openweathermap.org/data/2.5/weather?id=5128581&units=imperial&APPID=a40b4ca7691175cf366fcb7e19a3fbde',
+    dataType: 'json',
+    success: getWeatherDetails
+  });
+
+  // Parse API response for degrees fahrenheit in NYC
+  function getWeatherDetails(weather) {
+    console.log(weather);
+    let degrees = weather.main.temp.toString();
+    if (degrees.indexOf('.') > 0) {
+      degrees = degrees.substring(0, degrees.indexOf('.'));
+    }
+    document.getElementById('weather').innerHTML = 'Currently ' + degrees + '°F in NYC'
+  }
 };
